db4c083138f0d67eab370581c2c90a3ac56ca746
viet2799 <nguyenducviet2799@gmail.com>
2023-08-23 09:14:16 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Table, Input, Button } from 'antd';
 
-function App() {
+const App = () => {
+  const [rowData, setRowData] = useState([]);
+  const [colData, setColData] = useState([]);
+  const [outputData, setOutputData] = useState([]);
+
+  const handleAddRow = () => {
+    setRowData([...rowData, '']);
+  };
+
+  const handleAddCol = () => {
+    setColData([...colData, '']);
+  };
+
+  const handleInputChange = (e, rowIndex, colIndex) => {
+    const updatedRowData = [...rowData];
+    const updatedColData = [...colData];
+
+    updatedRowData[rowIndex] = e.target.value;
+    updatedColData[colIndex] = e.target.value;
+
+    setRowData(updatedRowData);
+    setColData(updatedColData);
+  };
+
+  const handleConfirm = () => {
+    const extractedData = rowData.map((rowValue, rowIndex) => {
+      const rowData = {};
+      colData.forEach((colValue, colIndex) => {
+        rowData[`cell${rowIndex}-${colIndex}`] = rowValue;
+      });
+      return { key: `row${rowIndex}`, ...rowData };
+    });
+    setOutputData(extractedData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={handleAddRow}>Thêm hàng</Button>
+      <Button onClick={handleAddCol}>Thêm cột</Button>
+      <Table
+        dataSource={rowData.map((row, rowIndex) => {
+          const rowData = {};
+          colData.forEach((col, colIndex) => {
+            rowData[`cell${rowIndex}-${colIndex}`] = '';
+          });
+          return { key: `row${rowIndex}`, ...rowData };
+        })}
+        columns={colData.map((col, colIndex) => ({
+          dataIndex: `cell${colIndex}-${rowData.length - 1}`,
+          key: `col${colIndex}`,
+          render: (text, record, rowIndex) => (
+            <Input
+              value={text}
+              onChange={(e) => handleInputChange(e, rowIndex, colIndex)}
+            />
+          ),
+        }))}
+        pagination={false}
+      />
+      <Button onClick={handleConfirm}>Xác nhận</Button>
+      <div>
+        <h2>Dữ liệu đã xác nhận:</h2>
+        <pre>{JSON.stringify(outputData, null, 2)}</pre>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
